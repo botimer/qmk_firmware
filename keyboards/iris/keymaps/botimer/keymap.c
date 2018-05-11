2059c92cfd14b2a214a18979f868d7dcacd969c5
Noah Botimer <botimer@umich.edu>
2018-05-11 19:41:43 -0400

    Set up a base qwerty layout
    I really enjoy the arrow key setup of the Pok3r and have taken to having
    a modifier under my left thumb without moving. This is a first attempt,
    with a fair bit of duplication, to accommodate normal typing and
    programming, making all of the symbols and numbers available below the
    top row (thinking somewhat about Let's Split compatibility).
    
    It is not quite symmetrical, but both raise and lower are easily reached
    with either hand, and gui/alt are side by side on the left hand for easy
    modifier chording. For usual typing, travel to the inside and outside
    columns is pretty well minimized. I might try to go back to the default
    of having shift on the home row for the left hand, but I have a ton of
    muscle memory from putting control on caps lock for years; it was quite
    difficult with the defaults when I tried it at first.

---
+++ keyboards/iris/keymaps/botimer/keymap.c
@@ -0,0 +1,144 @@
+#include "iris.h"
+#include "action_layer.h"
+#include "eeconfig.h"
+
+extern keymap_config_t keymap_config;
+
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+#define _ADJUST 16
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST,
+};
+
+#define KC_ KC_TRNS
+#define _______ KC_TRNS
+
+#define KC_LOWR LOWER
+#define KC_RASE RAISE
+#define KC_RST RESET
+#define KC_BL_S BL_STEP
+#define KC_DBUG DEBUG
+#define KC_RTOG RGB_TOG
+#define KC_RMOD RGB_MOD
+#define KC_RHUI RGB_HUI
+#define KC_RHUD RGB_HUD
+#define KC_RSAI RGB_SAI
+#define KC_RSAD RGB_SAD
+#define KC_RVAI RGB_VAI
+#define KC_RVAD RGB_VAD
+
+#define KC_AGRV LALT(KC_GRV)
+#define KC_ATAB RALT(KC_TAB)
+#define KC_AENT RALT(KC_ENT)
+#define KC_OBSP RGUI(KC_BSPC)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_QWERTY] = LAYOUT_kc(
+  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
+     ESC , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,GRV ,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+     TAB , Q  , W  , E  , R  , T  ,                Y  , U  , I  , O  , P  ,BSPC,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+     LCTL, A  , S  , D  , F  , G  ,                H  , J  , K  , L  ,SCLN,QUOT,
+  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
+     LSFT, Z  , X  , C  , V  , B  ,RASE,     ENT , N  , M  ,COMM,DOT ,SLSH,RSFT,
+  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
+                       LGUI,LALT,LOWR,         SPC ,RASE,LOWR
+  //                  `----+----+----'        `----+----+----'
+  ),
+
+  [_LOWER] = LAYOUT_kc(
+  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
+     AGRV,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,PSLS,NLCK,TILD,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+     ATAB,LABK,LCBR,RCBR,RABK,BSLS,               UNDS, P7 , P8 , P9 ,PLUS,    ,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+         ,LBRC,LPRN,RPRN,RBRC,PIPE,               EQL , P4 , P5 , P6 ,MINS,SPC ,
+  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
+         ,GRV ,QUOT,DQUO,COLN,QUES,    ,         , P0 , P1 , P2 , P3 ,PDOT,COMM,
+  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
+                           ,    ,    ,         ENT ,    ,
+  //                  `----+----+----'        `----+----+----'
+  ),
+
+  [_RAISE] = LAYOUT_kc(
+  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
+     F12 , F1 , F2 , F3 , F4 , F5 ,                F6 , F7 , F8 , F9 ,F10 ,F11 ,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+         ,EXLM, AT ,HASH,DLR ,WH_U,               VOLU,PGUP, UP ,PGDN,ESC ,    ,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+         ,PERC,CIRC,AMPR,ASTR,WH_D,               VOLD,LEFT,DOWN,RGHT,SPC ,ENT ,
+  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
+         ,TILD,BSLS,UNDS,QUES,APP ,    ,         ,MUTE,HOME,INS ,END ,DEL ,AENT,
+  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
+                           ,    ,    ,             ,    ,
+  //                  `----+----+----'        `----+----+----'
+  ),
+
+  [_ADJUST] = LAYOUT_kc(
+  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
+     RST ,PSCR,SLCK,PAUS,WAKE,SLEP,                   ,    ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+     RTOG,RMOD,RHUI,RSAI,RVAI,VOLU,                   ,    ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+     BL_S,DBUG,RHUD,RSAD,RVAD,VOLD,                   ,    ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
+     CAPS,MSTP,MPRV,MPLY,MNXT,MUTE,    ,         ,    ,    ,    ,    ,    ,    ,
+  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
+                           ,    ,    ,             ,    ,
+  //                  `----+----+----'        `----+----+----'
+  )
+
+};
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        persistent_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case ADJUST:
+      if (record->event.pressed) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
